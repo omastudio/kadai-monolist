849e45d6497ac7e0e557f6b210a737eaa69b3f16
EC2 Default User <ec2-user@ip-172-31-62-236.ec2.internal>
2019-03-26 07:13:11 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,11 @@ class ItemsController < ApplicationController
   
   before_action :require_user_logged_in
   
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+  
   def new
     #空の配列として初期化
     @items = []
@@ -16,28 +21,9 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
-  end
-  
-  private
-
-  def read(result)
-    code = result['itemCode']
-    name = result['itemName']
-    url = result['itemUrl']
-    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-    
-    #必要な値を読み出して、最後にハッシュとしてreturn
-
-    {
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
     delete 'logout', to: 'sessions#destroy'
 
     get 'signup', to: 'users#new'
-
     resources :users, only: [:show, :new, :create]
     
-    resources :items, only: [:new]
+    resources :items, only: [:show, :new]
+    resources :ownerships, only: [:create, :destroy]
 end
